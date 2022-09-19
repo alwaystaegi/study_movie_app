4a38045a52f6bbb7b8b2a93f6a4d3cb49b7082d3
alwaystaegi <ffff4565@naver.com>
2022-09-19 15:10:08 +0900

    ClickCounter

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
-import React from "react";
-// import Movies from "./components/Movies";
-// function App() {
-//   return <div>hello world</div>;
-// }
+import React, { useState } from "react";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
+function App() {
+  let [text, setText] = useState("");
+  function changed(ele) {
+    setText(ele.target.value);
   }
-  증가함수 = () => {
-    this.setState(function (현재스테이트) {
-      return { count: 현재스테이트.count + 1 };
-    });
-    //this.setState({ count: this.state.count + 1 });
-  };
-  감소함수 = () => {
-    this.setState(function (현재스테이트) {
-      return { count: 현재스테이트.count - 1 };
-    });
-  };
-  render() {
-    return (
-      <div>
-        <h1>카운터: {this.state.count}</h1>
-        <button onClick={this.증가함수}>+1</button>
-        <button onClick={this.감소함수}>-1</button>
-      </div>
-    );
-  }
+
+  return (
+    <>
+      <div>hello App</div>
+      <input
+        onChange={changed}
+        type={"text"}
+        placeholder="아이디를 입력하세요"
+        value={text}
+      />
+      {text ? <div>입력된 id:{text}</div> : null}
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/ClickCounter.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+function ClickCounter(props) {
+  let [count, setCount] = useState(0);
+  let [id, setID] = useState();
+  let [flag, setFlag] = useState();
+  function Timer() {
+    if (!flag) {
+      setFlag(true);
+      setID(
+        (id = setInterval(() => {
+          setCount(function (a) {
+            return a + 1;
+          });
+        }, 1000))
+      );
+    }
+  }
+
+  function Exit() {
+    setFlag(false);
+    clearInterval(id);
+    setID(undefined);
+  }
+
+  function Reset() {
+    setCount(0);
+    if (flag) {
+      clearInterval(id);
+      setID(
+        (id = setInterval(() => {
+          setCount(function (a) {
+            return a + 1;
+          });
+        }, 1000))
+      );
+    } else {
+      setID(undefined);
+    }
+  }
+  return (
+    <>
+      <div className="container">
+        <h1>{props.title?props.title:"시간 카운터"}</h1>
+        <div>Count : {count}</div>
+        <div>타이머의 ID : {id}</div>
+        <button onClick={Timer}>시작</button>
+        <button onClick={Exit}>종료</button>
+        <button onClick={Reset}>초기화</button>
+      </div>
+    </>
+  );
+}
+
+export default ClickCounter;
