51386a9060662cb6f6a6312d916c43be2e817399
alwaystaegi <ffff4565@naver.com>
2022-09-20 18:36:02 +0900

    유튜브 클론코딩...?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 //! react-router-dom 사용===> Link tag...> a tag와 비슷함 but 새로 필요한 요소만 가져옴 ex)만약 모든 페이지에 둘리가 기본으로 있다? 둘리는 새로 가져오지 않고 그대로 사용
 
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Movies from "./components/Movies";
 import Menu from "./components/Menu";
-import Youtube from "./components/Youtube";
+import Youtube from "./Youtube/Youtube_main";
 function Home() {
   return <h2>홈 컴포넌트</h2>;
 }
---
+++ src/Youtube/Youtube_main.js
@@ -0,0 +1,130 @@
+import "./Youtube.css";
+
+function Youtube(props) {
+  return (
+    <div className="all">
+      <div className="yt__main">
+        {/*//!상단바 */}
+        <div className="yt__top">
+          <div className="yt__top_item">
+            <span>5:10</span>
+          </div>
+          <div className="yt__top_item yt__top_item_left">
+            {/* //!와이파이아이콘 */}
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+                width="15px"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8.288 15.038a5.25 5.25 0 017.424 0M5.106 11.856c3.807-3.808 9.98-3.808 13.788 0M1.924 8.674c5.565-5.565 14.587-5.565 20.152 0M12.53 18.22l-.53.53-.53-.53a.75.75 0 011.06 0z"
+                />
+              </svg>
+            </span>
+            <span>LTE </span>
+            {/* //!배터리 아이콘 */}
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+                width="15px"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 10.5h.375c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125H21M4.5 10.5H18V15H4.5v-4.5zM3.75 18h15A2.25 2.25 0 0021 15.75v-6a2.25 2.25 0 00-2.25-2.25h-15A2.25 2.25 0 001.5 9.75v6A2.25 2.25 0 003.75 18z"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
+        <div className="yt__top1">
+          <div className="yt__top1_right">
+            <div className="yt__icon_text">🔞YOUTUBE</div>
+            {/* //todo 아이콘 만들기 해야한다 */}
+          </div>
+          <div className="yt__top1_left">
+            {/* //!공유아이콘 */}
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+                width="20px"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z"
+                />
+              </svg>
+            </div>
+            {/* //!알림아이콘 */}
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+                width="20px"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+                />
+              </svg>
+            </div>
+            {/* //!검색아이콘 */}
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+                width="20px"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+                />
+              </svg>
+            </div>
+            <div className="Profile">효택</div>
+          </div>
+        </div>
+        <div className="findbar">
+          {/* //todo */}
+          <div className="f">탐색</div>
+          <div className="findbar_index">
+            <div>all</div>
+            <div id="new">새로운 맞춤 동영상</div>
+            <div>live</div>
+            <div>music</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Youtube;
