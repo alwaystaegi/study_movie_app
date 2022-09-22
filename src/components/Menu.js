387ff4da50aed39d2147636284cb4b8dc5cead92
alwaystaegi <ffff4565@naver.com>
2022-09-22 15:14:49 +0900

    kakao clone page end

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,17 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Menu.css";
 function Menu() {
+  const [darkmode, setDark] = useState(false);
+  function toggle() {
+    setDark(!darkmode);
+    if (!darkmode) {
+      console.log("on");
+      document.querySelector("body").classList.add("dark");
+    } else document.querySelector("body").classList.remove("dark");
+  }
   return (
-    <nav>
+    <nav className="overflow-y-hidden ">
       <ul>
         <li className="">
           <Link to="/">홈으로</Link>
@@ -32,6 +41,17 @@ function Menu() {
         </li>
         <li className="">
           <Link to="/kakao">kakao</Link>
+        </li>
+        <li>
+          <div className="text-white">
+            <input
+              id="toggle_dark"
+              type="checkbox"
+              checked={darkmode}
+              onChange={toggle}
+            ></input>
+            <label htmlFor="toggle_dark">다크모드</label>
+          </div>
         </li>
       </ul>
     </nav>
